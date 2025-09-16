aa5882ea36c8529e688f70474c52b88a9aaee422
HuynhTruc97 <huynhtruc09072004@gmail.com>
2025-09-17 05:59:58 +0700

    Create store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import MainHeader from "./features/header/components/main-header/MainHeader";
 import { Categories } from "./features/header";
 import { CATEGORY_DATA } from "./features/header/components/categories/constants";
