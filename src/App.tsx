7143a5df18eb8178d5f3b3510777fdf3c0e14920
HuynhTruc97 <huynhtruc09072004@gmail.com>
2025-09-16 23:42:53 +0700

    Change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import "./App.css";
 import { Counter } from "./features/example-1/Counter";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddRecipes from "./features/add-recipes/AddRecipes";
-import MainHeader from "./features/header/components/main-header/MainHeader";
-import { Categories } from "./features/header";
-import { CATEGORY_DATA } from "./features/header/components/categories/constants";
 
 function App() {
   return (
@@ -14,8 +11,6 @@ function App() {
         <Route path="/counter-page" element={<Counter />} />
       </Routes>
       <>
-        <MainHeader />
-        <Categories items={CATEGORY_DATA} />
         <AddRecipes />
       </>
     </BrowserRouter>
