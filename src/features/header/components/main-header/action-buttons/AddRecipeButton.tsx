e7a6231256ff11a4c1c989a6b3ab56068d080fda
HuynhTruc97 <huynhtruc09072004@gmail.com>
2025-09-18 10:28:25 +0700

    Change CategoryItem

--- src/features/header/components/main-header/action-buttons/AddRecipeButton.tsx
+++ src/features/header/components/main-header/action-buttons/AddRecipeButton.tsx
@@ -1,18 +1,19 @@
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 
 type AddRecipeButtonProps = {
-	className?: string;
-	onClick?: () => void;
+  className?: string;
+  onClick?: () => void;
 };
 
 export default function AddRecipeButton({ onClick }: AddRecipeButtonProps) {
-	return (
-		<Button
-			variant="secondary"
-			size="lg"
-			className="w-full sm:w-auto h-12 sm:h-14 lg:h-16 xl:h-[48px] font-medium text-base sm:text-lg lg:text-xl xl:text-[22px] text-(--light-black-color) px-4 sm:px-5 py-3 sm:py-4 lg:py-5 xl:py-6 bg-(--gray-color) rounded-full cursor-pointer hover:bg-(--gray-hover-color) transition-colors"
-			onClick={onClick}>
-			<span style={{ fontFamily: 'var(--poppins-medium)' }}>+ ADD A RECIPE</span>
-		</Button>
-	);
+  return (
+    <Button
+      variant="secondary"
+      size="lg"
+      className="w-full sm:w-auto h-12 sm:h-14 lg:h-16 xl:h-[48px] font-medium text-base sm:text-lg lg:text-xl xl:text-[22px] text-(--light-black-color) px-4 sm:px-5 py-3 sm:py-4 lg:py-5 xl:py-6 bg-(--gray-color) rounded-full cursor-pointer hover:bg-(--gray-hover-color) transition-colors"
+      onClick={onClick}
+    >
+      <span>+ ADD A RECIPE</span>
+    </Button>
+  );
 }
