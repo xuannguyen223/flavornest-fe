0f3dba49a92d7be2b3ac8f100c51ba54ab937c3f
khienht <khanhhientyy03@gmail.com>
2025-09-18 14:33:28 +0700

    Add API service for Recipe and Setup env

--- src/services/recipe.service.ts
+++ src/services/recipe.service.ts
@@ -5,13 +5,30 @@ import axiosInstance from './axiosInstance';
 // dưới đây là ví dụ cách dùng, không cần phải viết lại axiosInstance trong file này
 // tham khảo cách viết
 
-// export const getAllRecipes = async () => {
-//     const response = await axiosInstance.get("/recipes", { withCredentials: true });
-//     return response.data;
-// };
-
 // viết dưới dạng lấy list theo query params (tham khảo thôi nhé)
 // export const getRecipesByCategory = async (category: string) => {
 //     const response = await axiosInstance.get(`/recipes?category=${category}`, { withCredentials: true });
 //     return response.data;
 // };
+
+// getAllRecipes
+export const getAllRecipes = async () => {
+    try {
+        const response = await axiosInstance.get(`/api/recipe/get`, { withCredentials: true });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching recipe detail:", error);
+        throw error;
+    }
+};
+
+// getRecipeById
+export const getRecipeById = async (id: string) => {
+    try {
+        const response = await axiosInstance.get(`/api/recipe/get/${id}`, { withCredentials: true });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching recipe detail:", error);
+        throw error;
+    }
+};
