2d93e3ee64af450cc5b76607784d042fe14175da
ycheryl <yulcheryl2357@gmail.com>
2025-09-22 16:36:37 +0700

    change loading page to spinner

--- src/features/loading/LoadingPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-      <div className="w-[30px] aspect-[1] rounded-full xl:scale-150 scale-110 animate-l5"></div>
-    </div>
-  );
-}
