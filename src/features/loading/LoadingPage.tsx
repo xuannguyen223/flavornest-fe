01fe1e88198ddb5d73b5a3c1be1bb664b4e938f4
ycheryl <yulcheryl2357@gmail.com>
2025-09-22 13:46:48 +0700

    add styles for loading page

---
+++ src/features/loading/LoadingPage.tsx
@@ -0,0 +1,7 @@
+export default function LoadingPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <div className="w-[30px] aspect-[1] rounded-full xl:scale-150 scale-110 animate-l5"></div>
+    </div>
+  );
+}
