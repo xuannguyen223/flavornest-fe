a51b7b668d56276d9e4c2675511b081ff0d3403c
khienht <khanhhientyy03@gmail.com>
2025-09-23 11:27:34 +0700

    [my-recipe]: Fix calling API

--- src/store/features/recipeAPISlice.tsx
+++ src/store/features/recipeAPISlice.tsx
@@ -48,13 +48,34 @@ export const fetchAllRecipes = createAsyncThunk('recipes/fetchAll', async () => 
 	return res as Recipe[];
 });
 
-export const fetchUserRecipes = createAsyncThunk(
-	'recipes/fetchByUserId',
-	async ({ userId }: { userId: string }) => {
-		const res = await getUserRecipes(userId);
-		return res as Recipe[];
-	},
-);
+export const fetchUserRecipes = createAsyncThunk<
+  Recipe[],
+  { userId: string },
+  { state: RootState }
+>('recipes/fetchByUserId', async ({ userId }) => {
+	const response = await getUserRecipes(userId);
+	const userRecipesData = response || [];
+
+	if (!userRecipesData.length) return [];
+
+	// Lấy full detail từng recipe
+	const recipePromises = userRecipesData.map((recipe: any) => {
+		const recipeId = recipe.id;
+		if (!recipeId) throw new Error(`Missing recipeId in user recipe: ${JSON.stringify(recipe)}`);
+		return getRecipeById(recipeId);
+	});
+
+	const recipeResponses = await Promise.all(recipePromises);
+	console.log("response", recipeResponses)
+	const detailedRecipes = recipeResponses.map(res => {
+		if (res.data?.recipe) return res.data.recipe;
+		if (res.recipe) return res.recipe;
+		return res;
+	});
+
+	return detailedRecipes as Recipe[];
+});
+
 
 export const fetchRecipesBySearch = createAsyncThunk(
 	'recipes/fetchBySearch',
