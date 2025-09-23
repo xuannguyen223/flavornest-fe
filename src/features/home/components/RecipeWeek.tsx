be6bba46cd0030358bbe3b6951cb939ee3de0ce3
ziuwuchuoi <vv0320nt@gmail.com>
2025-09-23 18:13:52 +0700

    Some UI

---
+++ src/features/home/components/RecipeWeek.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import RecipeCategories from '@/features/recipe-detail/components/RecipeCategories';
+import type { Recipe } from '@/types/TypeRecipe';
+import { useNavigate } from 'react-router-dom';
+
+interface RecipeWeekProps {
+	recipe?: Recipe;
+}
+
+export function RecipeWeek({ recipe }: RecipeWeekProps) {
+	const navigate = useNavigate();
+
+	if (!recipe) {
+		return null;
+	}
+
+	return (
+		<div className={`w-full mx-auto px-20 pb-20`}>
+			<div className="flex items-center justify-between mb-8">
+				<div className="text-5xl font-cormorant font-semibold text-gray-900 tracking-tight text-balance">
+					Recipe of the Week
+				</div>
+			</div>
+
+			<div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-start">
+				<div
+					className="relative h-[250px] md:h-[250px]"
+					onClick={() => navigate(`/recipes/${recipe.id}`)}>
+					<img
+						src={recipe.imageUrl || '/images/placeholder.png'}
+						className="w-full h-full object-cover shadow-sm rounded-lg"
+					/>
+				</div>
+
+				<div className="flex flex-col">
+					<div className="flex flex-col space-y-2 h-full">
+						<div className="text-2xl font-semibold text-gray-900">{recipe.title}</div>
+						<div className="flex items-center gap-2 text-lg">
+							By
+							<div className="font-semibold text-amber-500 text-lg">
+								{recipe.author.profile.name}
+							</div>
+						</div>
+
+						<div className="text-gray-700 leading-relaxed text-sm sm:text-base lg:text-lg xl:text-xl">
+							{recipe.description}
+						</div>
+					</div>
+					{/* <div>
+						<RecipeCategories
+							categories={recipe.categories.map(c => c.category)}
+							hasLabel={false}
+						/>
+					</div> */}
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/features/recipe-detail/components/RecipeCategories.tsx
+++ src/features/recipe-detail/components/RecipeCategories.tsx
@@ -2,40 +2,42 @@ import { useNavigate } from 'react-router-dom';
 import type { Category } from '@/types/TypeRecipe';
 
 interface RecipeCategoriesListProps {
-  categories: Category[];
+	categories: Category[];
+	hasLabel?: boolean;
 }
 
-export default function RecipeCategories({ categories }: RecipeCategoriesListProps) {
-  const navigate = useNavigate();
+export default function RecipeCategories({ categories, hasLabel }: RecipeCategoriesListProps) {
+	const navigate = useNavigate();
 
-  const handleClick = (name: string, description: string) => {
-    navigate(
-      `/recipes?category=${encodeURIComponent(name)}&desc=${encodeURIComponent(description)}`
-    );
-  };
+	const handleClick = (name: string, description: string) => {
+		navigate(
+			`/recipes?category=${encodeURIComponent(name)}&desc=${encodeURIComponent(description)}`,
+		);
+	};
 
-  return (
-    <section className="text-left mt-8">
-      {/* Header */}
-      <h2 className="text-sm sm:text-base lg:text-lg xl:text-xl font-semibold leading-tight text-neutral-700">
-        Tags
-      </h2>
+	return (
+		<section className="text-left mt-8">
+			{/* Header */}
+			{hasLabel && (
+				<h2 className="text-sm sm:text-base lg:text-lg xl:text-xl font-semibold leading-tight text-neutral-700">
+					Tags
+				</h2>
+			)}
 
-      {/* Category Item List */}
-      <div className="mt-4 flex flex-wrap gap-2">
-        {categories.map((cat) => (
-          <button
-            key={cat.id}
-            onClick={() => handleClick(cat.name, cat.description)}
-            className="px-6 py-2 rounded-full border 
+			{/* Category Item List */}
+			<div className="mt-4 flex flex-wrap gap-2">
+				{categories.map(cat => (
+					<button
+						key={cat.id}
+						onClick={() => handleClick(cat.name, cat.description)}
+						className="px-6 py-2 rounded-full border 
                        border-neutral-400/30 bg-white text-neutral-600 
                        shadow-sm hover:bg-neutral-700 hover:text-white transition
-                       text-xs sm:text-sm lg:text-base xl:text-lg "
-          >
-            {cat.name}
-          </button>
-        ))}
-      </div>
-    </section>
-  );
+                       text-xs sm:text-sm lg:text-base xl:text-lg ">
+						{cat.name}
+					</button>
+				))}
+			</div>
+		</section>
+	);
 }
